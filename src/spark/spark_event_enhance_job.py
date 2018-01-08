dccb9d32e8ab2f04acaeb7a6aaf490152a759cf4
Gustavo Ribeiro <gustavo.ribeiro@radixeng.com.br>
2018-01-08 01:17:34 -0200

    Added event processor

--- src/spark/spark_event_enhance_job.py
+++ src/spark/spark_event_enhance_job.py
@@ -18,7 +18,10 @@ def process_event(event):
     eventEnhancer = EventEnhancer()
     event = eventEnhancer.categorizeEvent(event)
     event = eventEnhancer.processGeolocation(event)
+    return event
 
-while True:    
-    result = sql.read.format("org.apache.spark.sql.cassandra").load(keyspace=CASSANDRA_KEYSPACE, table="events").select("*").where("location_level is null").orderBy(desc("timestamp_ms"))
-    result.rdd.foreach(process_event)
+#while True:    
+result = sql.read.format("org.apache.spark.sql.cassandra").load(keyspace=CASSANDRA_KEYSPACE, table="events").select("*").where("location_level is null").orderBy(desc("timestamp_ms")).limit(5)
+rs = result.collect()
+for row in rs:
+    process_event(row)
