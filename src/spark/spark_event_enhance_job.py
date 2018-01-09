9ad08057f3dec85a1bc6ce7ce003e5ccfc5dd5fa
Gustavo Ribeiro <gustavo.ribeiro@radixeng.com.br>
2018-01-09 01:57:15 -0200

    Fixed row processing for imutable objects

--- src/spark/spark_event_enhance_job.py
+++ src/spark/spark_event_enhance_job.py
@@ -4,6 +4,7 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
 from event_processor import EventEnhancer
+from pyspark.sql import Row
 
 DB_CONFIG = json.load(open('dbconfig.json'))
 CASSANDRA_HOST = DB_CONFIG["cassandra"]["host"]
@@ -18,10 +19,14 @@ def process_event(event):
     eventEnhancer = EventEnhancer()
     event = eventEnhancer.categorizeEvent(event)
     event = eventEnhancer.processGeolocation(event)
+    rdd = sc.parallelize([tuple(event.values())])
+    df = rdd.toDF(list(event.keys()))
+    df.write.format("org.apache.spark.sql.cassandra").mode('append').options(table="events", keyspace=CASSANDRA_KEYSPACE).save()
+    print(event)
     return event
 
 #while True:    
 result = sql.read.format("org.apache.spark.sql.cassandra").load(keyspace=CASSANDRA_KEYSPACE, table="events").select("*").where("location_level is null").orderBy(desc("timestamp_ms")).limit(5)
 rs = result.collect()
 for row in rs:
-    process_event(row)
+    process_event(row.asDict())
